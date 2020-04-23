8a48b4d728a33dd2056b934375f462ffb5af8f49
lenovo <lenovo@qq.com>
2020-04-23 20:00:27 +0800

    2020.4.23 20:00已提交

---
+++ atcrowdfunding-common/src/main/java/com/atguigu/atcrowdfunding/entity/User.java
@@ -0,0 +1,41 @@
+package com.atguigu.atcrowdfunding.entity;
+
+public class User {
+
+    private Integer id;
+    private String account;
+    private String username;
+    private String userpassword;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUserpassword() {
+        return userpassword;
+    }
+
+    public void setUserpassword(String userpassword) {
+        this.userpassword = userpassword;
+    }
+}
