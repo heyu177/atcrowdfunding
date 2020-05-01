23c4b4a5f73dc29dff8a6426036404341cc9b7b8
heyu <heyu@qq.com>
2020-05-01 23:30:28 +0800

    2020.5.1 23:29已提交

--- atcrowdfunding-manager/src/main/java/com/atguigu/atcrowdfunding/dao/UserMapper.java
+++ atcrowdfunding-manager/src/main/java/com/atguigu/atcrowdfunding/dao/UserMapper.java
@@ -1,8 +1,9 @@
 package com.atguigu.atcrowdfunding.dao;
 
 import com.atguigu.atcrowdfunding.entity.User;
-import org.apache.ibatis.annotations.Param;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 public interface UserMapper {
 
     public User getUserByAccountUserpassword(User user);
