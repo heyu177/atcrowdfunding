26a1b7eb75434a1f5210d1556208b41837ad75b9
lenovo <lenovo@qq.com>
2020-05-25 18:23:07 +0800

    2020.5.25 18:21已提交

--- atcrowdfunding-manager/src/main/java/com/atguigu/atcrowdfunding/service/PermissionService.java
+++ atcrowdfunding-manager/src/main/java/com/atguigu/atcrowdfunding/service/PermissionService.java
@@ -9,4 +9,6 @@ public interface PermissionService {
     public Permission queryRootPermission();
 
     public List<Permission> queryChildPermissions(Integer pid);
+
+    public void insertPermission(Permission permission);
 }
